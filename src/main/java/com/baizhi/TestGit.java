6ac8265c469bf31066ca383d663a0ecff0f6bf76
xml <2530684123@qq.com>
2019-08-27 14:54:48 +0800

    new2.0

--- src/main/java/com/baizhi/TestGit.java
+++ src/main/java/com/baizhi/TestGit.java
@@ -3,6 +3,6 @@ package com.baizhi;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("11111111111");
-
+        System.out.println("22222222222");
     }
 }
